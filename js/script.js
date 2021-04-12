21662ce20170e5d8cacc393aab4c6ce37137693e
Aite Aigbe <aite@nabla.ooo>
2021-04-12 19:48:50 -0400

    nav revision

--- js/script.js
+++ js/script.js
@@ -12,16 +12,46 @@ let i = parseFloat(margin.marginBottom);
 let logoPos = (intViewportHeight - (logoHeight.bottom + i));
 const mediaQuery = window.matchMedia('(min-width: 768px)')
 
+
+// Get the modal
+var modal = document.getElementsByClassName("modal")[0];
+
+// Get the button that opens the modal
+var btn = document.getElementById("m4");
+
+// Get the <span> element that closes the modal
+var span = document.getElementById("close");
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
+
 window.onload = (event) => {
     anime({
         targets: menu,
         easing: 'easeInOutExpo',
-        translateY: "100vh"
+        translateY: "100vh",
+        duration: 1
     }); 
     anime({
         targets: logo2,
         easing: 'easeInOutExpo',
-        translateY: "100vh"
+        translateY: "100vh",
+        duration: 1
     });
 };
 
@@ -64,7 +94,7 @@ function butClick() {
                 anime({
                     targets: logo2,
                     easing: 'cubicBezier(0.075, 0.820, 0.165, 1.000)',
-                    translateY: '50vh',
+                    translateY: '42vh',
                     duration: 300
                 });
                 wrap.style.display = "grid";
@@ -130,28 +160,3 @@ handleTabletChange(mediaQuery)
 
 
 
-// Get the modal
-var modal = document.getElementsByClassName("modal")[0];
-
-// Get the button that opens the modal
-var btn = document.getElementById("m4");
-
-// Get the <span> element that closes the modal
-var span = document.getElementById("close");
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-};
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-};
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-};
