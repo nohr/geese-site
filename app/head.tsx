19353c014c4677c3e7cea2eb31fd2fb6221c3f53
Aite Aigbe <aite@nabla.ooo>
2023-01-11 20:26:19 +0100

    theme color

--- app/head.tsx
+++ app/head.tsx
@@ -3,6 +3,7 @@ export default function Head() {
     <>
       <title>Geese</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="theme-color" content="#7D0000" />
       <meta name="description" content="3D Country" />
       <link rel="icon" href="/favicon.ico" />
     </>
