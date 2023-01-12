02572542ee1d03b8dc1dcbaeba9aa3a03f0dfda2
Aite Aigbe <aite@nabla.ooo>
2023-01-12 02:04:40 +0100

    completed pubsub cloud function

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 import Links from "./(ui)/links";
 import Canvas from "./(ui)/canvas";
+import generate from "../functions/src/dalle";
 
 export default async function Home() {
+  // generate();
+  // console.log(await generate());
+
   return (
     <div className="flex h-full flex-col items-center justify-center px-8">
       <h1 className="font-serif text-xl font-normal">GEESE</h1>
