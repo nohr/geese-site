b7ee220ea1d8f8b0e46aada445d7fccd65fed0a0
Aite Aigbe <aite@nabla.ooo>
2023-01-12 14:23:31 +0100

    disable caching in prod

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+export const dynamic = 'auto',
+dynamicParams = true,
+revalidate = 0,
+fetchCache = 'only-no-store',
+runtime = 'nodejs',
+preferredRegion = 'auto'
+
 import Links from "./(ui)/links";
 import Canvas from "./(ui)/canvas";
 import { pickImage } from "./(api)/api";
