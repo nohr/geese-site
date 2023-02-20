8695c4e6c7e666117eec2ecde23527830b8faeb9
Aite Aigbe <aite@nabla.ooo>
2023-02-20 12:34:44 +0100

    integrate partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 import { astroImageTools } from "astro-imagetools";
+import partytown from "@astrojs/partytown";
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), astroImageTools],
+  integrations: [react(), tailwind(), astroImageTools, partytown()],
   output: "server",
   adapter: vercel()
 });
